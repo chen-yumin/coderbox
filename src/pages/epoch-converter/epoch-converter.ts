b635a2137c8073cf00166503c85d7cdba9297461
Chen Yumin <hello@chenyumin.com>
2017-07-23 15:01:04 +0100

    Add more description on home page

--- src/pages/epoch-converter/epoch-converter.ts
+++ src/pages/epoch-converter/epoch-converter.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'epoch-converter',
   templateUrl: 'epoch-converter.html'
 })
 export class EpochConverterPage {
+  currentTime: number;
   epochTime: number;
   isoTimeString: string;
 
   constructor(public navCtrl: NavController) {
+    let timer = Observable.timer(0, 1000);
+    timer.subscribe(t => this.currentTime = Math.round(new Date().getTime()/1000.0));
     this.epochTime = Math.round(new Date().getTime()/1000.0);
     this.updateTime();
   }
