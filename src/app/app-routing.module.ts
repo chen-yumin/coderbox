543b26c8bf7f53484ebcfae20499dec21d6b87a2
Yumin Chen <hello@chenyumin.com>
2019-05-14 23:38:23 +0100

    Remove List page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
   { 
     path: 'hash-generator', 
     loadChildren: './hash-generator/hash-generator.module#HashGeneratorPageModule' 
